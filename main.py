a47badb2ba6bef1188aae11f86d0a48a2641ba6a
Gall-oDrone <piehavok@hotmail.com>
2025-02-08 19:50:44 -0600

    Building and Running Graph

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ from typing import List, Sequence
 import datetime
 
 from dotenv import load_dotenv
+from graph.graph import app
 load_dotenv()
+
 
 if __name__ == "__main__":
     print("Hello Advanced RAG flows")
+    print(app.invoke(input={"question": "what is agent memory?"}))
     
